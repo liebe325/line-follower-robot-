52e5e31b339cdf442f4e3d29b04497176283c77d
Muruali <168755691+liebe325@users.noreply.github.com>
2026-01-14 19:58:08 +0530

    Update sketch_oct7a_copy_20241008213616_copy_20241_copy_20241009201905.ino

--- sketch_oct7a_copy_20241008213616_copy_20241_copy_20241009201905/sketch_oct7a_copy_20241008213616_copy_20241_copy_20241009201905.ino
+++ sketch_oct7a_copy_20241008213616_copy_20241_copy_20241009201905/sketch_oct7a_copy_20241008213616_copy_20241_copy_20241009201905.ino
@@ -1,131 +1,214 @@
-#include <QTRSensors.h>
+/*
+Basic Code Template for the LF-2 robot using 8 channel Analog Sensor
+*/
 
-QTRSensors qtr;                        //qtr sensor array
-const uint8_t SensorCount = 8;
-uint16_t sensorValues[SensorCount];
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
 
+//--------Pin definitions for the TB6612FNG Motor Driver----
+#define AIN1 4
+#define BIN1 6
+#define AIN2 3
+#define BIN2 7
+#define PWMA 9
+#define PWMB 10
+//------------------------------------------------------------
 
-//var for motor driver
-int ael=9;                        //el-elable is a value bet 0 to 255 for speed
-int aph=6;                        //ph-phase is direction of the motor
-int bel=5;                        
-int bph=3;
-int mode=8;                       //mode is operating mode with the values lke speed which in per(%)
+//--------Enter Line Details here---------
+bool isBlackLine = 1;             //keep 1 in case of black line. In case of white line change this to 0
+unsigned int numSensors = 8;      
+//-----------------------------------------
 
+int P, D, I, previousError, PIDvalue;
+double error;
+int lsp, rsp;
+int lfSpeed = 170;//100
+int currentSpeed = 170;//100
+int sensorWeight[8] = { 8, 4, 2, 1, -1, -2, -4, -8 };
+int activeSensors;
+float Kp = 0.02; //0.02
+float Kd = 0.95;  //0.65
+float Ki = 0.001; //0.01
 
-int button_Cal=2;                 //calibrate
-int button_NF=7;                  //on and off
-
-
-int lastError
-double Kp=0.09;
-double Ki=0;
-double Kd=0.9;
-
+int onLine = 1;
+int minValues[8], maxValues[8], threshold[8], sensorValue[8], sensorArray[8];
 
 void setup() {
 
-  qtr.setTypeRC();
-  qtr.setSensorPins((const uint8_t[]){10,11,12,A0,A1,A2,A3,A4,A5}, SensorCount);
-  qtr.setEmitterPin(7);
+  sbi(ADCSRA, ADPS2);
+  cbi(ADCSRA, ADPS1);
+  cbi(ADCSRA, ADPS0);
 
-  pinMode(ael, OUTPUT);
-  pinMode(aph, OUTPUT);
-  pinMode(bel, OUTPUT);
-  pinMode(bph, OUTPUT);
-  pinMode(mode, OUTPUT);
-  pinMode(button_Cal,INPUT);
-  pinMode(button_NF,INPUT);
-  digitalWrite(mode, HIGH);
+  Serial.begin(9600);
 
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
+  pinMode(PWMA, OUTPUT);
+  pinMode(PWMB, OUTPUT);
+  pinMode(11, INPUT_PULLUP);  //Pushbutton
+  pinMode(12, INPUT_PULLUP);  //Pushbutton
+  pinMode(13, OUTPUT);        //LED
 
-  while(digialRead(button_Cal)==LOW){}
-  calibratesensor()
-  while(digialRead(button_NF)==LOW){}
-
+  pinMode(5, OUTPUT);     //standby for older carrier boards
+  digitalWrite(5, HIGH);  //enables the motor driver
+                          //in case of a newer board with motor enable jumper, you can delete these two lines which frees up digital pin 5
 
 }
 
 void loop() {
-   PID()
+  // put your main code here, to run repeatedly:
+  while (digitalRead(11)) {}
+  delay(1000);
+  calibrate();
+  while (digitalRead(12)) {}
+  delay(1000);
+
+  while (1) {
+    readLine();
+    if (currentSpeed < lfSpeed) currentSpeed++;
+    if (onLine == 1) {  //PID LINE FOLLOW
+      linefollow();
+      digitalWrite(13, HIGH);
+    } else {
+      digitalWrite(13, LOW);
+      if (error < -1000) {
+        motor2run(0);
+        motor1run(200);
+      } else if (error > 1000) {
+        motor2run(200);
+        motor1run(0);
+      }
+    }
+  }
 }
 
-void PID(){
-  uint16_t positionLine=qtr.readLineBlack(sensorValues);
-  int error = 3500 - positionLine;
+void linefollow() {
+  error = 0;
+  activeSensors = 0;
 
-  int P = error;
-  int I = error+I;
-  int D = error-lastError;
+  for (int i = 0; i < 8; i++) {
+    error += sensorWeight[i] * sensorArray[i] * sensorValue[i];
+    activeSensors += sensorArray[i];
+  }
+  error = error / activeSensors;
 
-  lastError = error;
-  
-  int motorspeedchange=(P*Kp)+(I*Ki)+(D*kp);
 
-  int motorSpeedA = 100 + motorspeedchange;
-  int motorSpeedB = 100 - motorspeedchange;
+  P = error;
+  I = I + error;
+  D = error - previousError;
 
-  if (motorSpeedA > 255){
-    motorSpeedA=200
+  PIDvalue = (Kp * P) + (Ki * I) + (Kd * D);
+  previousError = error;
+
+  lsp = currentSpeed - PIDvalue;
+  rsp = currentSpeed + PIDvalue;
+
+  if (lsp > 255) {
+    lsp = 255;
   }
-  if (motorSpeedB > 255){
-    motorSpeedB=200
+  if (lsp < 0) {
+    lsp = 0;
   }
-  if (motorSpeedA < -75){
-    motorSpeedA=-75
+  if (rsp > 255) {
+    rsp = 255;
   }
-  if (motorSpeedB < -75){
-    motorSpeedB =-75
+  if (rsp < -70) {
+    rsp = -70;
   }
-  
-  motor_movement(motorSpeeA,motorSpeeA);
+  //Serial.print(rsp);
+  //Serial.print("   ");
+  //Serial.println(lsp);
+  motor2run(rsp);
+  motor1run(lsp);
 }
 
+void calibrate() {
+  for (int i = 0; i < 8; i++) {
+    minValues[i] = analogRead(i);
+    maxValues[i] = analogRead(i);
+  }
 
-//calibrate
+  for (int i = 0; i < 10000; i++) {
+    motor1run(70);
+    motor2run(-70);
 
-void calibratesensor(){
+    for (int i = 0; i < 8; i++) {
+      if (analogRead(i) < minValues[i]) {
+        minValues[i] = analogRead(i);
+      }
+      if (analogRead(i) > maxValues[i]) {
+        maxValues[i] = analogRead(i);
+      }
+    }
+  }
 
-  for (uint16_t i = 0; i < 400; i++)
-  {
-    digialWrite(aph,50);
-    digialWrite(bph,-50);
-    qtr.calibrate();
+  for (int i = 0; i < 8; i++) {
+    threshold[i] = (minValues[i] + maxValues[i]) / 2;
+    Serial.print(threshold[i]);
+    Serial.print(" ");
+  }
+  Serial.println();
 
-  }  
-
+  motor2run(0);
+  motor1run(0);
 }
 
+void readLine() {
+  onLine = 0;
 
-// speeda - speed of motor a 
-// speedb - speed of motor b
-void motor_movement(int speedA,int speedB){      // speeda - speed of motor a and speedb - speed of motor b
-  
-  //motor A backward or forward
-  if (speedA < 0){
+  for (int i = 0; i < 8; i++) {
+    if (isBlackLine) {
+      sensorValue[i] = map(analogRead(i), minValues[i], maxValues[i], 0, 1000);
+    } else {
+      sensorValue[i] = map(analogRead(i), minValues[i], maxValues[i], 1000, 0);
+    }
+    sensorValue[i] = constrain(sensorValue[i], 0, 1000);
+    sensorArray[i] = sensorValue[i] > 500;
 
-    speedA=0-speedA                              //making the value +ve for the input
-    digitalWrite(aph,HIGH);
-
+    //Serial.print(sensorValue[i]);
+    //Serial.print("  ");
+    if (sensorArray[i]) onLine = 1;
   }
-  else{
-    digitalWrite(aph,LOW);
+  //Serial.println();
+}
+//--------Function to run Motor 1-----------------
+void motor1run(int motorSpeed) {
+  motorSpeed = constrain(motorSpeed, -255, 255);
+  if (motorSpeed > 0) {
+    digitalWrite(AIN1, 1);
+    digitalWrite(AIN2, 0);
+    analogWrite(PWMA, motorSpeed);
+  } else if (motorSpeed < 0) {
+    digitalWrite(AIN1, 0);
+    digitalWrite(AIN2, 1);
+    analogWrite(PWMA, abs(motorSpeed));
+  } else {
+    digitalWrite(AIN1, 1);
+    digitalWrite(AIN2, 1);
+    analogWrite(PWMA, 0);
   }
+}
 
-
-  //motor B backward or forward
-  if (speedB < 0){
-
-    speedB=0-speedB
-    digitalWrite(bph,HIGH);
-
+//--------Function to run Motor 2-----------------
+void motor2run(int motorSpeed) {
+  motorSpeed = constrain(motorSpeed, -255, 255);
+  if (motorSpeed > 0) {
+    digitalWrite(BIN1, 1);
+    digitalWrite(BIN2, 0);
+    analogWrite(PWMB, motorSpeed);
+  } else if (motorSpeed < 0) {
+    digitalWrite(BIN1, 0);
+    digitalWrite(BIN2, 1);
+    analogWrite(PWMB, abs(motorSpeed));
+  } else {
+    digitalWrite(BIN1, 1);
+    digitalWrite(BIN2, 1);
+    analogWrite(PWMB, 0);
   }
-  else{
-    digitalWrite(bph,LOW);
-  }  
- 
-  analogWrite(ael,speedA);        // motor A
-  analogWrite(bel,speedA);        // motor B
-  
-
 }
